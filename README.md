81c139e7b0555cbcbfae32591d165d52188fe239
sskong777 <sskong777@gmail.com>
2022-07-25 01:24:40 +0900

    push-notification

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # android_pjt
+
+## 1. BMI 측정 
+
+* 키, 몸무게 입력 받아서 BMI 수치와 비만도 계산 화면 구현
+
+
+
+## 2. Push Notification
+
+* firebase Clound Messaging 사용
+  * https://console.firebase.google.com/?hl=ko
+  * token값 받아오기
+
---
+++ pushnotification/app/src/main/java/com/example/MyFirebaseMessagingService.kt
@@ -0,0 +1,15 @@
+package com.example
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+    }
+}
