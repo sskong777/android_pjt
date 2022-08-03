e311246777dca8e43e1df046d52c05f5c9039f08
sskong777 <sskong777@gmail.com>
2022-08-04 02:39:54 +0900

    modi health connect api

--- healthconnecttest/app/src/main/java/com/example/aop/part2/health_connect_test/MainActivity.kt
+++ healthconnecttest/app/src/main/java/com/example/aop/part2/health_connect_test/MainActivity.kt
@@ -3,17 +3,42 @@ package com.example.aop.part2.health_connect_test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.health.connect.client.HealthConnectClient
+import androidx.health.platform.client.proto.PermissionProto
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val PERMISSIONS =
+            setOf(
+                PermissionProto.Permission.createReadPermission(HeartRateSeries::class),
+                Permission.createWritePermission(HeartRateSeries::class),
+                Permission.createReadPermission(Steps::class),
+                Permission.createWritePermission(Steps::class)
+            )
+
+// Create the permissions launcher.
+        val requestPermissions =
+            registerForActivityResult(HealthDataRequestPermissions()) { granted ->
+                if (granted.containsAll(PERMISSIONS)) {
+                    // Permissions successfully granted
+                } else {
+                    // Lack of required permissions
+                }
+            }
+
+
+
         if (HealthConnectClient.isAvailable(context)) {
             // Health Connect is available and installed.
             val healthConnectClient = HealthConnectClient.getOrCreate(context)
         } else {
-            // ...
+
         }
+
     }
+
 
 }
