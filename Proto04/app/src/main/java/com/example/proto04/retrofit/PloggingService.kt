bb79fce45665cd626eee92872f12b36ca0a1469a
SSAFY <sskong777@gmail.com>
2022-08-11 17:58:21 +0900

    fragment

--- Proto04/app/src/main/java/com/example/proto04/retrofit/PloggingService.kt
+++ Proto04/app/src/main/java/com/example/proto04/retrofit/PloggingService.kt
@@ -9,14 +9,14 @@ import retrofit2.http.POST
 
 interface PloggingService {
 
-        @Headers("X-Auth-Token: eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiJoZWxsb0BnbWFpbC5jb20iLCJlbWFpbCI6ImhlbGxvQGdtYWlsLmNvbSIsImlhdCI6MTY2MDEyOTk5MywiZXhwIjoxNjYwMTQ0MzkzfQ.ykXPLBd_6ZGmNvitMp1V05nd29V2K92Y99466fZgaEg",
+        @Headers("X-Auth-Token: eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiJ0anJndXM3NzdAdGpyZ3VzLmNvbSIsImVtYWlsIjoidGpyZ3VzNzc3QHRqcmd1cy5jb20iLCJpYXQiOjE2NjAxOTc3MjcsImV4cCI6MTY2MDIxMjEyN30.6lLSLdVtOrbkfoMxMbjGT7VU0XxA9hBWUG9hqPKJuZY",
         "Content-Type: application/json")
         @POST("plogging")
         fun addPloggingByEnqueue(@Body ploggingInfo: RequestBody): Call<PloggingResponseBody> // Call 은 흐름처리 기능을 제공해줌
 
 
 
-    @Headers("X-Auth-Token: eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiJoZWxsb0BnbWFpbC5jb20iLCJlbWFpbCI6ImhlbGxvQGdtYWlsLmNvbSIsImlhdCI6MTY2MDEzNzIwNCwiZXhwIjoxNjYwMTUxNjA0fQ.qGWm-TrURK44ofevY_GRNrBZTCny3iR2E8XGV63BSLw",
+    @Headers("X-Auth-Token: eyJ0eXBlIjoiand0IiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiJ0anJndXM3NzdAdGpyZ3VzLmNvbSIsImVtYWlsIjoidGpyZ3VzNzc3QHRqcmd1cy5jb20iLCJpYXQiOjE2NjAxOTc3MjcsImV4cCI6MTY2MDIxMjEyN30.6lLSLdVtOrbkfoMxMbjGT7VU0XxA9hBWUG9hqPKJuZY",
         "Content-Type: application/json")
     @GET("plogging/all")
     fun getPlogging(): Call<PloggingGetResponseBody> // Call 은 흐름처리 기능을 제공해줌
